2a7e93ccd3288d4014e4be78606856450393dd94
tbibby49 <timbibbens@hotmail.com>
2024-12-24 11:38:59 +1100

    rolled back importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,8 +6,9 @@ pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js"
 
 pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.8
 pin "@stimulus-components/sortable", to: "@stimulus-components--sortable.js" # @5.0.1
 pin "sortablejs" # @1.15.2
+pin "popper", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"
+pin "jquery", to: "https://code.jquery.com/jquery-3.5.1.min.js"
